d8748445caf003a68758aa2add6ce7dee9a2e8dc
jhb0430 <leky2224@gmail.com>
2024-12-24 18:53:10 +0900

    [배지현] -header footer menu 구성

--- src/main/java/com/jhb0430/nunubooks/user/UserController.java
+++ src/main/java/com/jhb0430/nunubooks/user/UserController.java
@@ -1,5 +1,18 @@
 package com.jhb0430.nunubooks.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/nunubooks/account")
+@Controller
 public class UserController {
+	
+	@GetMapping("/sign-up")
+	public String joinUser() {
+		
+		return "account/sign-up";
+	}
+	
 
 }
