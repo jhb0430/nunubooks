fb697c88f1fb690297fbd3c1739b5354a4067f35
jhb0430 <leky2224@gmail.com>
2025-01-02 18:49:58 +0900

    [배지현] - service package 분리

--- src/main/java/com/jhb0430/nunubooks/books/service/BookService.java
+++ src/main/java/com/jhb0430/nunubooks/books/service/BookService.java
@@ -1,8 +1,11 @@
 package com.jhb0430.nunubooks.books.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.jhb0430.nunubooks.aladdin.AladdinOpenAPI;
+import com.jhb0430.nunubooks.books.domain.Books;
 
 @Service
 public class BookService {
@@ -15,9 +18,10 @@ public class BookService {
 	
 	
 	
-	public List<> searchBooks() {
+	public List<Books> searchBooks(String searchWord) {
 		
-	
+
+		// GetUrl = > http://www.aladdin.co.kr/ttb/api/ItemSearch.aspx? + searchWord 
 	}
 	
 	
