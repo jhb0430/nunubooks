5a216c079691d351482413fdb5057d6ad3de7fb3
jhb0430 <leky2224@gmail.com>
2025-01-06 00:13:18 +0900

    [배지현] - list thymeleaf 출력 테스트 #7

--- src/main/java/com/jhb0430/nunubooks/books/BookController.java
+++ src/main/java/com/jhb0430/nunubooks/books/BookController.java
@@ -1,8 +1,15 @@
 package com.jhb0430.nunubooks.books;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.jhb0430.nunubooks.books.dto.BookDTO;
+
+import reactor.core.publisher.Mono;
 
 @RequestMapping("/nunubooks")
 @Controller
@@ -22,5 +29,42 @@ public class BookController {
 		return "books/product";
 	}
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
+	@GetMapping("/searchList")
+	public String bookList(
+					Model model
+							) {
+		 WebClient webClient = webClientBuilder.build();
+		 
+		 Mono<BookDTO> response = 
+				 webClient.get()
+					.uri(
+					"https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?"
+					+ "ttbkey=ttbleky22241703001"
+					+ "&"
+					+ "Query=aladdin"
+					+ "&"
+					+ "QueryType=Title"
+					+ "&"
+					+ "MaxResults=10"
+					+ "&"
+					+ "start=1"
+					+ "&"
+					+ "SearchTarget=Book"
+					+ "&"
+					+ "output=js"
+					+ "&"
+					+ "Version=20131101"
+					) 
+			.retrieve()
+			.bodyToMono(BookDTO.class);
+		 
+		 BookDTO bookDTO = response.block();
+		 model.addAttribute("books",bookDTO.getItem());
+		 
+	    return "books/searchList";
+	}
 	
 }
--- src/main/java/com/jhb0430/nunubooks/books/BookRestController.java
+++ src/main/java/com/jhb0430/nunubooks/books/BookRestController.java
@@ -47,7 +47,7 @@ public class BookRestController {
 	
 	@GetMapping("/nunubooks/test")
 	public BookDTO bookTest(
-//			@RequestParam("Query") String Query
+//			@RequestParam("query") String query
 			)  {
 		
 		 WebClient webClient = webClientBuilder.build();
@@ -58,11 +58,22 @@ public class BookRestController {
 //			webClient.get().uri("https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbleky22241703001&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101&"
 //			+ Query //&Query=aladdin
 			webClient.get()
+			// uriBuilder  사용법 검색해보기
+/*
+			 메서드	설명	예시
+scheme()	URL의 프로토콜을 설정합니다.		https, http
+host()	도메인 이름을 설정합니다.				www.aladin.co.kr
+port()	포트 번호를 설정합니다.				8080
+path()	경로를 설정합니다.					/ttb/api/ItemSearch.aspx
+queryParam()	쿼리 파라미터를 추가합니다.		Query=aladdin
+build()	설정한 값들을 조합해 최종 URI를 생성합니다.	https://www.aladin.co.kr/...
+			 * */
 					.uri(
 					"https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?"
 					+ "ttbkey=ttbleky22241703001"
 					+ "&"
 					+ "Query=aladdin"
+//					+ "Query=" + query
 					+ "&"
 					+ "QueryType=Title"
 					+ "&"
@@ -78,9 +89,10 @@ public class BookRestController {
 					) 
 			.retrieve()
 			.bodyToMono(BookDTO.class);
-	    
 	    return response.block();
-		
 	}
+
+
+
 	
 }
