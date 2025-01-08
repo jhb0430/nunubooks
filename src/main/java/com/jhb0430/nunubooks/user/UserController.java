260201f38730980c4c0cb9bef0b286c51ac0541c
jhb0430 <leky2224@gmail.com>
2025-01-09 00:42:35 +0900

    [배지현] -로그인시 우측 상단에 회원정보 표시기능 - 로그아웃 기능 추가 완료 #2

--- src/main/java/com/jhb0430/nunubooks/user/UserController.java
+++ src/main/java/com/jhb0430/nunubooks/user/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/nunubooks/account")
 @Controller
 public class UserController {
@@ -26,6 +29,17 @@ public class UserController {
 		return "books/main";
 	}
 
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		
+		return "redirect:/nunubooks/account/main";
+	}
 	
 	
 
