e1275e6119cab3bfdef38f87c62bbd7fe0f06735
jhb0430 <leky2224@gmail.com>
2025-01-21 18:25:21 +0900

    [배지현] - aws 배포 코드 적용

--- src/main/java/com/jhb0430/nunubooks/NunuBooksApplication.java
+++ src/main/java/com/jhb0430/nunubooks/NunuBooksApplication.java
@@ -2,9 +2,10 @@ package com.jhb0430.nunubooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NunuBooksApplication {
+public class NunuBooksApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NunuBooksApplication.class, args);
