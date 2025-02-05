6f691ac0f693832005ce716f12ca5aea93c028c7
jhb0430 <leky2224@gmail.com>
2025-02-05 18:16:02 +0900

    [배지현] -permissionInterseptoer 수정중 -WebMvcConfig 생성

---
+++ src/main/java/com/jhb0430/nunubooks/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.jhb0430.nunubooks.config;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.jhb0430.nunubooks.Intercepter.PermissionInterceptor;
+
+public class WebMvcConfig implements WebMvcConfigurer{
+
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**");
+//		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+
+	
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new PermissionInterceptor())
+		.addPathPatterns("/**");
+//		.excludePathPatterns("/user/logout", "/static/**", "/images/**");
+		
+		
+		
+	}
+	
+	
+}
