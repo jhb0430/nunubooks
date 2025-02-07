3d8eed435e6211076a59a737038461161257bb71
jhb0430 <leky2224@gmail.com>
2025-02-07 17:19:53 +0900

    [배지현] -WebMvc Intercepter 적용 완료

--- src/main/java/com/jhb0430/nunubooks/config/WebMvcConfig.java
+++ src/main/java/com/jhb0430/nunubooks/config/WebMvcConfig.java
@@ -1,19 +1,21 @@
 package com.jhb0430.nunubooks.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.jhb0430.nunubooks.Intercepter.PermissionInterceptor;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
 	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**");
-//		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
-	}
+//	@Override -> 정적 파일들의 경로를 잡아주는 메서드
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/images/**");
+////		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+//	}
 
 	
 	
