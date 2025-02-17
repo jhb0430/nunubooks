17f9f8e83b466b80e8730bf031628cd1c7710def
jhb0430 <leky2224@gmail.com>
2025-02-17 17:11:21 +0900

    [배지현] - SMTP 설정 2

---
+++ src/main/java/com/jhb0430/nunubooks/email/service/EmailService.java
@@ -0,0 +1,22 @@
+package com.jhb0430.nunubooks.email.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EmailService {
+
+	private final JavaMailSender javaMailSender;
+	
+	public void sendSimpleMailMessage() {
+		//https://baby9235.tistory.com/131
+		//https://velog.io/@tjddus0302/Spring-Boot-%EB%A9%94%EC%9D%BC-%EB%B0%9C%EC%86%A1-%EA%B8%B0%EB%8A%A5-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-Gmail
+		//https://blogan99.tistory.com/101
+		// 소리지르고싶다
+	}
+}
