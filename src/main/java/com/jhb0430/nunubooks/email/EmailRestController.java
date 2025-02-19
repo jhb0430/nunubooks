36d694bafbf4b734cb93526126d725e018787e59
jhb0430 <leky2224@gmail.com>
2025-02-19 18:54:01 +0900

    [배지현] - 임시 비밀번호 발급 페이지 수정중 - email 발송 로직에서 임시 비밀번호 창을 통해 추가 정보 가져오는 과정에서의 오류 디버깅 진행중 #12

--- src/main/java/com/jhb0430/nunubooks/email/EmailRestController.java
+++ src/main/java/com/jhb0430/nunubooks/email/EmailRestController.java
@@ -1,24 +1,39 @@
 package com.jhb0430.nunubooks.email;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jhb0430.nunubooks.email.service.EmailService;
+import com.jhb0430.nunubooks.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/nunubooks")
 @RequiredArgsConstructor
 public class EmailRestController {
 
 		    private final EmailService mailService;
+		    private final UserService userService;
 
-		    @GetMapping("/send-email")
-		    public void sendMimeMessage(@RequestParam("email") String email) {
-		        mailService.sendMimeMessage(email);
+		    @PostMapping("/send-email")
+		    public Map<String, Boolean> sendMimeMessage(
+			    		@RequestParam("name") String name
+			    		,@RequestParam("loginId") String loginId
+			    		,@RequestParam("email") String email
+			    		) {
+		    	
+		    	Map<String, Boolean> resultMap = new HashMap<>();
+		    	if(userService.findUserforSetPw(name, loginId, email)) {
+		    		mailService.sendMimeMessage(email);
+		    		resultMap.put("result",true );
+		    	} else {
+		    		resultMap.put("result",false );
+		    	}
+		    	return resultMap;
 		    }
 	
 }
